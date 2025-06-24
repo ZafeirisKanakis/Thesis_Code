da9c794375cf4770456a6f2483ed39045c231e77
ZafeirisKanakis <142676746+ZafeirisKanakis@users.noreply.github.com>
2025-06-24 13:20:06 +0300

    Add files via upload
    This code is based on the original SimpleNet (https://github.com/DonaldRR/SimpleNet implementation, with minor modifications to resolve dependency issues and ensure compatibility with modern Python libraries.

---
+++ SimpleNet/test.py
@@ -0,0 +1,22 @@
+# import torch
+# print(torch.cuda.is_available())
+# print(torch.cuda.get_device_name(0))
+# import torch
+# print(torch.__version__)
+# import torch
+# print(torch.version.cuda)
+# import torch
+
+# print("PyTorch version:", torch.__version__)
+# print("CUDA available:", torch.cuda.is_available())
+# print("CUDA version:", torch.version.cuda)
+# print("Number of GPUs:", torch.cuda.device_count())
+# if torch.cuda.is_available():
+#     print("Current GPU:", torch.cuda.get_device_name(0))
+
+
+
+import torch
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
